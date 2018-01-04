08eed0781920edcdfa69c815febfbf3c9043c784
duncanmckee <duncan@mckee.net>
2018-01-04 11:56:40 -0800

    Begin math library

---
+++ Assets/Scripts/Math.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Math<N>{
+
+	public static N Sign(N number)
+    {
+        return (number > 0) ? 1 : ((number < 0) ? -1 : 0);
+    }
+
+    public static N AbsoluteValue(N number)
+    {
+        return (number >= 0) ? number : (-1 * number);
+    }
+
+}
